a6ed69285dc441a0394dbdcbe4a7ed6345c59c0f
Georgi Anastasov <lqlqgeorgi@abv.bg>
2022-06-25 18:43:43 +0300

    Parsed class without response

--- src/main/scala/JsonParserCurrent.scala
+++ src/main/scala/JsonParserCurrent.scala
@@ -12,56 +12,56 @@ import java.io.{FileInputStream, FileOutputStream}
 import javax.swing.text.DefaultFormatter
 
 case class Location(
-  name: String,
-  region: String,
-  country: String,
-  lat: Double,
-  lon: Double,
-  tz_id: String,
-  localtime_epoch: Int,
-  localtime: String
-):
-  def myArgs: List[Tuple2[String, Object]] =
+                     name: String,
+                     region: String,
+                     country: String,
+                     lat: Double,
+                     lon: Double,
+                     tz_id: String,
+                     localtime_epoch: Int,
+                     localtime: String
+                   ):
+  def myArgs: List[(String, Object)] =
     this.productElementNames.toList
       .map(i => i -> this.getClass.getDeclaredField(i).get(this))
 
 case class Current(
-  last_updated_epoch: Int,
-  last_updated: String,
-  temp_c: Double,
-  temp_f: Double,
-  is_day: Int,
-  condition: Condition,
-  wind_mph: Double,
-  wind_kph: Double,
-  wind_degree: Int,
-  wind_dir: String,
-  pressure_mb: Double,
-  pressure_in: Double,
-  precip_mm: Double,
-  precip_in: Double,
-  humidity: Int,
-  cloud: Int,
-  feelslike_c: Double,
-  feelslike_f: Double,
-  vis_km: Double,
-  vis_miles: Double,
-  uv: Double,
-  gust_mph: Double,
-  gust_kph: Double
-):
-  def myArgs: List[Tuple2[String, Object]] =
+                    last_updated_epoch: Int,
+                    last_updated: String,
+                    temp_c: Double,
+                    temp_f: Double,
+                    is_day: Int,
+                    condition: Condition,
+                    wind_mph: Double,
+                    wind_kph: Double,
+                    wind_degree: Int,
+                    wind_dir: String,
+                    pressure_mb: Double,
+                    pressure_in: Double,
+                    precip_mm: Double,
+                    precip_in: Double,
+                    humidity: Int,
+                    cloud: Int,
+                    feelslike_c: Double,
+                    feelslike_f: Double,
+                    vis_km: Double,
+                    vis_miles: Double,
+                    uv: Double,
+                    gust_mph: Double,
+                    gust_kph: Double
+                  ):
+  def myArgs: List[(String, Object)] =
     this.condition.myArgs ++ this.productElementNames.toList
       .filter(!_.equals("condition"))
       .map(i => i -> this.getClass.getDeclaredField(i).get(this))
 
 case class Condition(text: String, icon: String, code: Int):
-  def myArgs: List[Tuple2[String, Object]] =
+  def myArgs: List[(String, Object)] =
     this.productElementNames.toList
       .map(i => i -> this.getClass.getDeclaredField(i).get(this))
 
 case class CurrentResult(location: Location, current: Current):
-  def myArgs: List[Tuple2[String, Object]] = this.current.myArgs ++ this.location.myArgs
+  def myArgs: List[(String, Object)] = this.current.myArgs ++ this.location.myArgs
 
 abstract class JsonParser[A]:
   val rawValue: String
@@ -93,129 +93,120 @@ abstract class JsonParser[A]:
     inputStream.close()
     workbook.close()
 
-class JsonParsedCurrent(response: Response) extends JsonParser[CurrentResult]:
-  val rawValue: String = response.text()
+class JsonParsedCurrent(override val rawValue: String,
+                        override val header: Map[String, Seq[String]]) extends JsonParser[CurrentResult] :
   val parsedValue: CurrentResult =
     implicit lazy val formats: Formats = DefaultFormats
 
     val json = parse(rawValue)
     json.extract[CurrentResult]
   val command: String = "current"
-  val header: Map[String, Seq[String]] = response.headers
 
 case class Astro(
-  sunrise: String,
-  sunset: String,
-  moonrise: String,
-  moonset: String,
-  moon_phase: String,
-  moon_illumination: String
-):
-  def myArgs: List[Tuple2[String, Object]] =
+                  sunrise: String,
+                  sunset: String,
+                  moonrise: String,
+                  moonset: String,
+                  moon_phase: String,
+                  moon_illumination: String
+                ):
+  def myArgs: List[(String, Object)] =
     this.productElementNames.toList
       .map(i => i -> this.getClass.getDeclaredField(i).get(this))
 
 case class Astronomy(astro: Astro):
-  def myArgs: List[Tuple2[String, Object]] = this.astro.myArgs
+  def myArgs: List[(String, Object)] = this.astro.myArgs
 
 case class AstronomyResult(location: Location, astronomy: Astronomy):
-  def myArgs: List[Tuple2[String, Object]] = this.location.myArgs ++ this.astronomy.myArgs
+  def myArgs: List[(String, Object)] = this.location.myArgs ++ this.astronomy.myArgs
 
-class JsonParsedAstronomy(response: Response) extends JsonParser[AstronomyResult]:
-  val rawValue: String = response.text()
-
+class JsonParsedAstronomy(override val rawValue: String,
+                          override val header: Map[String, Seq[String]]) extends JsonParser[AstronomyResult] :
   val parsedValue: AstronomyResult =
     implicit lazy val formats: Formats = DefaultFormats
 
     val json = parse(rawValue)
     json.extract[AstronomyResult]
   val command: String = "astronomy"
-  val header: Map[String, Seq[String]] = response.headers
 
-class JsonParsedTimeZone(response: Response) extends JsonParser[Location]:
-
-  val rawValue: String = response.text()
+class JsonParsedTimeZone(override val rawValue: String,
+                         override val header: Map[String, Seq[String]]) extends JsonParser[Location] :
   val parsedValue: Location =
     implicit lazy val formats: Formats = DefaultFormats
 
     val json = parse(rawValue)
     json.extract[Location]
   val command: String = "timezone"
-  val header: Map[String, Seq[String]] = response.headers
 
 case class Matches(
-  stadium: String,
-  country: String,
-  region: String,
-  tournament: String,
-  start: String,
-  `match`: String
-):
-  def myArgs: List[Tuple2[String, Object]] =
+                    stadium: String,
+                    country: String,
+                    region: String,
+                    tournament: String,
+                    start: String,
+                    `match`: String
+                  ):
+  def myArgs: List[(String, Object)] =
     this.productElementNames.toList
       .map(i => i -> this.getClass.getDeclaredField(i).get(this))
 
-class JsonParsedFootball(response: Response) extends JsonParser[List[Matches]]:
-
-  val rawValue: String = response.text()
+class JsonParsedFootball(override val rawValue: String,
+                         override val header: Map[String, Seq[String]]) extends JsonParser[List[Matches]] :
   val parsedValue: List[Matches] =
     implicit lazy val formats: Formats = DefaultFormats
 
     val json = parse(rawValue) \ "football"
     json.extract[List[Matches]]
   val command: String = "football"
-  val header: Map[String, Seq[String]] = response.headers
 
 case class Hour(
-  time_epoch: Int,
-  time: String,
-  temp_c: Double,
-  temp_f: Double,
-  is_day: Int,
-  condition: Condition,
-  wind_mph: Double,
-  wind_kph: Double,
-  wind_degree: Int,
-  wind_dir: String,
-  pressure_mb: Double,
-  pressure_in: Double,
-  precip_mm: Double,
-  precip_in: Double,
-  humidity: Int,
-  cloud: Int,
-  feelslike_c: Double,
-  feelslike_f: Double,
-  windchill_c: Double,
-  windchill_f: Double,
-  heatindex_c: Double,
-  heatindex_f: Double,
-  dewpoint_c: Double,
-  dewpoint_f: Double,
-  will_it_rain: Int,
-  chance_of_rain: Int,
-  will_it_snow: Int,
-  chance_of_snow: Int,
-  vis_km: Double,
-  vis_miles: Double,
-  gust_mph: Double,
-  gust_kph: Double,
-  uv: Double
-):
-  def myArgs: List[Tuple2[String, Object]] =
+                 time_epoch: Int,
+                 time: String,
+                 temp_c: Double,
+                 temp_f: Double,
+                 is_day: Int,
+                 condition: Condition,
+                 wind_mph: Double,
+                 wind_kph: Double,
+                 wind_degree: Int,
+                 wind_dir: String,
+                 pressure_mb: Double,
+                 pressure_in: Double,
+                 precip_mm: Double,
+                 precip_in: Double,
+                 humidity: Int,
+                 cloud: Int,
+                 feelslike_c: Double,
+                 feelslike_f: Double,
+                 windchill_c: Double,
+                 windchill_f: Double,
+                 heatindex_c: Double,
+                 heatindex_f: Double,
+                 dewpoint_c: Double,
+                 dewpoint_f: Double,
+                 will_it_rain: Int,
+                 chance_of_rain: Int,
+                 will_it_snow: Int,
+                 chance_of_snow: Int,
+                 vis_km: Double,
+                 vis_miles: Double,
+                 gust_mph: Double,
+                 gust_kph: Double,
+                 uv: Double
+               ):
+  def myArgs: List[(String, Object)] =
     this.condition.myArgs ++ this.productElementNames.toList
       .filter(!_.equals("condition"))
       .map(i => i -> this.getClass.getDeclaredField(i).get(this))
 
-class JsonParsedForecast(response: Response) extends JsonParser[List[Hour]]:
-
-  val rawValue: String = response.text()
+class JsonParsedForecast(override val rawValue: String,
+                         override val header: Map[String, Seq[String]]) extends JsonParser[List[Hour]] :
   val parsedValue: List[Hour] =
     implicit lazy val formats: Formats = DefaultFormats
 
     val json = parse(rawValue) \\ "hour"
     json.extract[List[Hour]]
   val command: String = "forecast"
-  val header: Map[String, Seq[String]] = response.headers
 
   def temperatures: List[Double] = parsedValue.map(_.temp_c)
 
